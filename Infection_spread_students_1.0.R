e6b8ac08823292464399d241e39c4aae43792885
laura3621 <lauraandres99@gmail.com>
2024-04-23 08:45:48 +0200

    Update Infection_spread_students_1.0.R

--- Infection_spread_students_1.0.R
+++ Infection_spread_students_1.0.R
@@ -1,16 +1,27 @@
-# set some variables
+#### set some variables
 beta <- 0.3
 students <- 25
-transmission_dist <- 2 #in meters
-# hall <- matrix(0, nrow = 6, ncol = 5)
+transmission_dist <- 1 #in meters
 random_absence <- 0.05
 lectures <- 1 #per week
+initial_prob=0.1
 
-# make a dataframe
+##### built the class room
+nrows <- 5 
+ncols <- 6
+seats <- expand.grid(rows=1:nrows, cols=1:ncols) 
+seats$ID <- 1:nrow(seats)
+health$location <- sample(seats$ID, students, replace=FALSE)
+
+# assign students to seats
+health$row <- seats$rows[health$location]
+health$col <- seats$cols[health$location]
+
+#### make a dataframe
 health <- data.frame(
   individual = 1:25,
-  infection_status = rep(0, students),
-  location = rep(0, students),
+  infection_status = 0,
+  location = 0,
   missed_days = 0, 
   past_affections = 0, 
   immunity = 0)
@@ -23,11 +34,4 @@ health$infection_status[sample(1:students, exp_infection_at_start)] <- 1
 
 # assign starting location with random abscenses
 health$location[sample(1:students, round(students * random_absence))] <- 1
-
-##### built the classroom 
-nrows <- 5 
-ncols <- 6
-
-
-
 
