2be2cd630b58cb27045a7e1e69fad522617a907d
reler0221 <167177174+reler0221@users.noreply.github.com>
2024-04-29 14:24:31 +0200

    Update get_transmissable_distance.R

--- get_transmissable_distance.R
+++ get_transmissable_distance.R
@@ -1,14 +1,15 @@
-beta=0.3
+beta=0.8
 
+## This is a function that calculates maximum transmissible distance from transmission rate(or beta)
 get_transmissable_distance=function(transmission_rate, threshold){
   p=transmission_rate
   i=0
   while(p>threshold){
     p=p/(2^i)
     i=i+1
-    #print(paste(p,i))
+    print(paste(p,i))
   }
   return(i)
 }
 
-distance=get_transmissable_distance(beta, threshold = 0.001)
+distance=get_transmissable_distance(beta, threshold = 0.01)
