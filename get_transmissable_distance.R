b1805442e2fffa23b8b8fa1afde2611a2cea3cbd
reler0221 <167177174+reler0221@users.noreply.github.com>
2024-04-29 12:42:07 +0200

    Update get_transmissable_distance.R

--- get_transmissable_distance.R
+++ get_transmissable_distance.R
@@ -1,14 +1,15 @@
 beta=0.3
+threshold=0.001
 
-get_transmissable_distance=function(transmission_rate){
+get_transmissable_distance=function(transmission_rate, threshold){
   p=transmission_rate
   i=0
-  while(p>0.001){
+  while(p>threshold){
     p=p/(2^i)
     i=i+1
-    print(paste(p,i))
+    #print(paste(p,i))
   }
   return(i)
 }
 
-distance=get_transmissable_distance(beta)
+distance=get_transmissable_distance(beta, threshold)
